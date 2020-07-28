0f99337d28eec87dda35ce240fa9262d4dd50061
Tobias Johansson <tobias@podme.com>
2020-07-28 20:38:01 +0200

    fix

--- handler.js
+++ handler.js
@@ -1,4 +1,3 @@
-const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
 const express = require("express");
 const chalk = require("chalk");
@@ -8,53 +7,52 @@ const config = require("./config");
 const TradingBot = require("./tradingBot");
 
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.json({ strict: false }));
 
+let intervalObj = {};
+
 app.get("/", async (req, res) => {
-    const strategy = new TradingBot();
+    const promiseArray = [];
+    let results = {
+        BTCUSDT: null,
+        ETHUSDT: null,
+        LTCUSDT: null,
+    };
+    console.log(results);
 
-    await strategy.run();
+    config.forEach((item) => {
+        const promise = new Promise((resolve, reject) => {
+            const strategy = new TradingBot();
+            strategy
+                .collectTradeData(item.symbol)
+                .then(() => {
+                    resolve();
+                    intervalObj[item.symbol] = setIntervalAsync(async () => {
+                        results[item.symbol] = await strategy.run(item);
+                    }, 1000);
+                })
+                .catch((err) => {
+                    clearIntervalAsync(intervalObj[item.symbol]);
+                    reject(err);
+                });
+        });
 
-    res.send("Trade check finished");
-});
-
-const promiseArray = [];
-let results = {
-    BTCUSDT: null,
-    ETHUSDT: null,
-    LTCUSDT: null,
-};
-console.log(results);
-
-config.forEach((item) => {
-    const promise = new Promise((resolve, reject) => {
-        const strategy = new TradingBot();
-        let interval = null;
-        strategy
-            .collectTradeData(item.symbol)
-            .then(() => {
-                resolve();
-                interval = setIntervalAsync(async () => {
-                    results[item.symbol] = await strategy.run(item);
-                }, 1000);
-            })
-            .catch((err) => {
-                clearIntervalAsync(interval);
-                reject(err);
-            });
+        promiseArray.push(promise);
     });
 
-    promiseArray.push(promise);
+    Promise.all(promiseArray);
+
+    res.send("Trading started");
 });
 
-Promise.all(promiseArray);
+app.get("/stop", (req, res) => {
+    config.forEach((item) => {
+        clearIntervalAsync(intervalObj[item.symbol]);
+    });
 
-setInterval(() => {
-    console.log(results);
-}, 10000);
+    res.send("Trading stopped");
+});
 
-// const strategy = new TradingBot();
-// strategy.getLastBuy("BTCUSDT");
-
-module.exports.trade_api = serverless(app);
+app.listen(port, () => console.log(`Running at port:${port}`));

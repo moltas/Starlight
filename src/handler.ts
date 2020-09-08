ef1c43665896e9950e856b1234fe379c848e6d90
Tobias Johansson <tobias@podme.com>
2020-09-08 21:10:31 +0200

    added endpoint for getting data

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,15 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { setIntervalAsync, clearIntervalAsync } from "set-interval-async/dynamic";
+import fs from "fs";
+import path from "path";
 
 import config from "./config";
 import TradingBot from "./tradingBot";
 import BinanceClient from "./clients/binanceClient";
 import BinanceClientMocked from "./clients/binanceClient_mocked";
 import { ConfigItem } from "./model";
+import { asyncForEach } from "./utils";
 
 const app = express();
 const port = 5000;
@@ -44,6 +47,24 @@ app.get("/stop", (req, res) => {
     });
 
     res.send("Trading stopped");
+});
+
+app.get("/data", async (req, res) => {
+    try {
+        const symbols = ["BTCUSDT", "ETHUSDT", "LTCUSDT", "LINKUSDT"];
+        const data = {};
+
+        await asyncForEach(symbols, async (symbol: string) => {
+            const filepath = path.resolve(`output/${symbol}_backtest.json`);
+            const fileContent = await fs.promises.readFile(filepath, "utf-8");
+            const fileObj = JSON.parse(fileContent);
+            data[symbol] = fileObj;
+        });
+
+        res.send(data);
+    } catch (err) {
+        res.send(err);
+    }
 });
 
 app.listen(port, () => console.log(`Running at port:${port}`));
